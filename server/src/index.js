98ef75d1762ecc3fe0f5a550c5e47ecdf26c0ecf
remixvit <remixvit@gmail.com>
2026-05-24 19:45:08 +0700

    fix: correct frontend dist path in production

--- server/src/index.js
+++ server/src/index.js
@@ -42,9 +42,9 @@ app.use('/api/agents', agentsRouter);
 
 // Serve frontend in production
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/dist')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../client/dist/index.html'));
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
 
